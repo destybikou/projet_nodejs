4eb959cd9f473e6065aa7eba081260701b30cd50
atacanjacques <atacan.jacques@gmail.com>
2019-12-10 15:01:37 +0100

    Route sans express

--- src/app.js
+++ src/app.js
@@ -6,8 +6,18 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  // res.end('Hello World');
+
+  let {url} = req;
+  // let url = req.url
+
+  if(url === "/"){
+    res.end("Home")
+  }
+  else if(url === "/posts"){
+    res.send("liste des articles")
+  }
+  
 });
 
 server.listen(port, hostname);
-
