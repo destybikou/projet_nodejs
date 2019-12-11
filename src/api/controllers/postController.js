a8c2f043255e3ecee3e926d516bb8b1cb3f5c5e6
atacanjacques <atacan.jacques@gmail.com>
2019-12-11 16:02:42 +0100

    Add more endpoints

--- src/api/controllers/postController.js
+++ src/api/controllers/postController.js
@@ -25,8 +25,50 @@ exports.create_a_post = (req, res) => {
       res.json({message: "Erreur serveur."});
     }
     else {
+      res.status(201);
+      res.json(post);
+    }
+  })
+}
+
+exports.get_a_post = (req, res) => {
+  Post.findById(req.params.post_id, (error, post) => {
+    if(error){
+      res.status(500);
+      console.log(error);
+      res.json({message: "Erreur serveur."});
+    }
+    else {
       res.status(200);
       res.json(post);
+    }
+  })
+}
+
+exports.update_a_post = (req, res) => {
+  Post.findOneAndUpdate({_id: req.params.post_id}, req.body, {new: true}, (error, post) => {
+    if(error){
+      res.status(500);
+      console.log(error);
+      res.json({message: "Erreur serveur."});
+    }
+    else {
+      res.status(200);
+      res.json(post);
+    }
+  })
+}
+
+exports.delete_a_post = (req, res) => {
+  Post.remove({_id: req.params.post_id}, (error) => {
+    if(error){
+      res.status(500);
+      console.log(error);
+      res.json({message: "Erreur serveur."});
+    }
+    else {
+      res.status(200);
+      res.json({message: "Article supprimé"});
     }
   })
 }
--- src/api/routes/postRoute.js
+++ src/api/routes/postRoute.js
@@ -5,8 +5,8 @@ module.exports = (app) => {
   .get(postController.list_all_posts)
   .post(postController.create_a_post);
 
-  // app.route('/posts/:id')
-  // .get(postController.get_a_post)
-  // .put(postController.update_a_post)
-  // .delete(postController.delete_a_post);
+  app.route('/posts/:post_id')
+  .get(postController.get_a_post)
+  .put(postController.update_a_post)
+  .delete(postController.delete_a_post);
 }
